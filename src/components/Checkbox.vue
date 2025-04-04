75ca66a251cfea74e158244e1ebd15cb1ad9d3b8
a104437ana <a104437@alunos.uminho.pt>
2025-04-04 17:06:14 +0100

    Add CheckBox

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,58 @@
+<!--
+    Copyright 2025 Ana Oliveira, Humberto Gomes, Inês Marques, Rafael Vilas Boas, Sara Lopes
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+-->
+
+<template>
+    <label class="custom-checkbox">
+        <input type="checkbox" v-model="checked" ref="checkbox" />
+        <span>{{ labelText }}</span>
+    </label>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const props = defineProps<{
+    labelText?: string;
+    initialChecked?: boolean;
+    indeterminate?: boolean;
+}>();
+
+const checked = ref(props.initialChecked ?? false);
+const checkbox = ref<HTMLInputElement | null>(null);
+
+onMounted(() => {
+    if (checkbox.value && props.indeterminate) {
+        checkbox.value.indeterminate = true;
+        checked.value = false;
+    }
+});
+</script>
+
+<style scoped>
+.custom-checkbox {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+}
+input[type="checkbox"]:indeterminate {
+    accent-color: rgb(222, 165, 165);
+}
+input[type="checkbox"] {
+    width: 20px;
+    height: 20px;
+    accent-color: var(--color-uminho);
+}
+</style>
