69f8f783bfdd0ecc95f575a1a8ec48ee779fdce5
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2025-04-17 10:21:26 +0100

    Fixed exception handling

--- db-copy.js
+++ db-copy.js
@@ -1,7 +1,3 @@
 import { copyFile } from 'fs/promises';
 
-try {
-    await copyFile('db.json', '.db-new.json');
-} catch (err) {
-    console.error('Error copying file:', err);
-}
+await copyFile('db.json', '.db-new.json');
