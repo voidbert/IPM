4b1e39908628cf8ecf5da85f5c699486d8373db5
voidbert <humbertogilgomes@protonmail.com>
2025-04-19 02:38:13 +0100

    Layout login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -16,30 +16,121 @@
 
 <template>
     <Navbar type="login" />
-    <main>Hello, world!</main>
-    <TextInput type="text-input" placeholder="Email" />
-    <TextInput type="search" placeholder="Password" />
-    <Button type="action">
-        <p><b>Iniciar Sessão</b></p>
-    </Button>
+    <main>
+        <form class="login-form">
+            <h1 class="page-title">Iniciar Sessão</h1>
+            <TextInput
+                type="text"
+                v-model="email"
+                placeholder="Email"
+                v-on:update:modelValue="resetWarning" />
+            <TextInput
+                type="password"
+                v-model="password"
+                placeholder="Password"
+                v-on:update:modelValue="resetWarning" />
+            <Checkbox v-model="rememberUser">Lembrar-se de mim</Checkbox>
+
+            <div class="login-submit-container">
+                <div class="warning-container">
+                    <Warning v-if="wrongCredentials">Email / password incorreto</Warning>
+                </div>
+                <Button
+                    @click="login"
+                    :type="email && password ? 'action' : 'disabled'"
+                    reasonDisabled="Preencha primeiro o email e a password">
+                    Iniciar Sessão
+                </Button>
+            </div>
+        </form>
+    </main>
 </template>
 
 <style scoped>
 main {
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+
     padding: 10px;
 }
+
+.login-form {
+    width: 25rem;
+    display: flex;
+    flex-direction: column;
+}
+
+.page-title {
+    display: flex;
+    justify-content: center;
+    margin-top: 0px; /* For correct vertical centering */
+
+    font-size: 1.5rem;
+}
+
+.login-submit-container {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+
+    margin-top: 1rem;
+}
+
+.warning-container {
+    display: flex;
+    flex: 2;
+}
+
 Button {
-    /* Just for testing purposes, change later */
-    position: absolute;
-    width: 10vw;
-    height: 5vh;
-    left: 50vw;
-    bottom: 30vh;
+    flex: 1;
 }
 </style>
 
 <script setup lang="ts">
+import Button from "../components/Button.vue";
+import Checkbox from "../components/Checkbox.vue";
 import Navbar from "../components/Navbar.vue";
 import TextInput from "../components/TextInput.vue";
-import Button from "../components/Button.vue";
+import Warning from "../components/Warning.vue";
+
+import { User } from "../models/User.ts";
+
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const email = ref("");
+const password = ref("");
+const rememberUser = ref(false);
+const wrongCredentials = ref(false);
+
+const resetWarning = () => {
+    wrongCredentials.value = false;
+};
+
+const login = async (e: FormDataEvent) => {
+    e.preventDefault();
+    // router.push('LoginPage');
+    const user = await User.tryAuthenticate(email.value.toLowerCase(), password.value);
+    if (user) {
+        switch (user.type) {
+            case "student":
+                router.replace("/MySchedule");
+                break;
+
+            case "director":
+                router.replace("/SolveProblems");
+                break;
+
+            default:
+                break;
+        }
+    } else {
+        wrongCredentials.value = true;
+        password.value = "";
+    }
+};
 </script>
