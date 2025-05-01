398168db0a343bddd7059a173db66f6ca14a6e88
voidbert <humbertogilgomes@protonmail.com>
2025-05-01 04:49:29 +0100

    Notification model update

--- src/models/Notification.ts
+++ src/models/Notification.ts
@@ -14,102 +14,71 @@
 
 import { fetchJson } from "./Utils.ts";
 
-export type NotificationType = "student" | "director" | "request" | "professor";
-
-export type ExchangeType = "shift" | "room" | "none";
-
-export type State = "pending" | "accepted" | "rejected";
+export type NotificationType = "studentRequest" | "professorRequest";
+export type NotificationState = "pending" | "accepted" | "rejected";
 
 export class Notification {
     // Common attributes
-    public id: number;
-    public to: number;
-    public from: number;
-    public content: string;
-    public date: Date;
-    public exchange: ExchangeType;
+    id: number;
+    from: number;
+    to: number;
+    date: Date;
+    course: number;
+    type: NotificationType;
+    state: NotificationState;
 
-    // Student and Director only attributes
-    public read?: boolean;
+    // Student request attributes
+    fromShift?: number;
+    toShift?: number;
 
-    // Director only attributes
-    public state?: State;
+    // Professor request attributes
+    currentShift?: number;
 
     constructor(
-        to: number,
+        id: number,
         from: number,
-        content: string,
-        date: Date | string,
-        read: boolean | undefined = undefined,
-        state: State | undefined = undefined,
-        exchange: ExchangeType = "none",
-        id: number = -1
+        to: number,
+        date: Date,
+        type: NotificationType,
+        state: NotificationState,
+        course: number,
+        fromShift: number | undefined = undefined,
+        toShift: number | undefined = undefined,
+        currentShift: number | undefined = undefined
     ) {
         this.id = id;
         this.to = to;
         this.from = from;
-        this.content = content;
-        this.date = typeof date === "string" ? new Date(date) : date;
-        this.exchange = exchange;
-        if (read !== undefined) {
-            this.read = read;
-        }
-        if (state !== undefined) {
-            this.state = state;
-        }
+        this.date = date;
+        this.type = type;
+        this.state = state;
+        this.course = course;
+        this.fromShift = fromShift;
+        this.toShift = toShift;
+        this.currentShift = currentShift;
     }
 
     static createFromObject(notificationObject: any): Notification {
         return new Notification(
-            notificationObject["to"],
+            Number(notificationObject["id"]),
             notificationObject["from"],
-            notificationObject["content"],
-            notificationObject["date"],
-            notificationObject["read"],
+            notificationObject["to"],
+            new Date(notificationObject["date"]),
+            notificationObject["type"],
             notificationObject["state"],
-            notificationObject["exchange"],
-            notificationObject["id"]
+            notificationObject["course"],
+            notificationObject["fromShift"],
+            notificationObject["toShift"],
+            notificationObject["currentShift"]
         );
     }
 
-    static async getUserNotifications(userId: number): Promise<Notification[]> {
+    async update(): Promise<void> {
+        await fetchJson(`/notifications/${this.id}`, "PUT", this);
+    }
+
+    static async getToUser(userId: number): Promise<Notification[]> {
         const notifications = (await fetchJson(`/notifications?to=${userId}`)) as any[];
         return notifications.map((notification) => Notification.createFromObject(notification));
-    }
-
-    static async getUserRequests(userId: number): Promise<Notification[]> {
-        const requests = (await fetchJson(`/notifications?from=${userId}`)) as any[];
-        const filteredRequests = requests.filter((request) => !(request.state === undefined));
-        return filteredRequests.map((request) => Notification.createFromObject(request));
-    }
-
-    static async setNotificationRead(id: number, read: boolean): Promise<Notification> {
-        const notification = await fetchJson(`/notifications/${id}`, "PATCH", { read: read });
-        return Notification.createFromObject(notification);
-    }
-
-    static async setNotificationState(id: number, state: State): Promise<Notification> {
-        const notification = await fetchJson(`/notifications/${id}`, "PATCH", { state: state });
-        return Notification.createFromObject(notification);
-    }
-
-    static async addNotification(
-        to: number,
-        from: number,
-        content: string,
-        type: NotificationType
-    ): Promise<Notification> {
-        const date = new Date();
-        let notification: Notification;
-        if (type === "student") {
-            notification = new Notification(to, from, content, date, false);
-        } else if (type === "director") {
-            notification = new Notification(to, from, content, date, false, "pending");
-        } else if (type === "request") {
-            notification = new Notification(to, from, content, date);
-        } else {
-            throw new Error(`Invalid notification type: ${type}`);
-        }
-        return await fetchJson(`/notifications`, "POST", notification);
     }
 }
